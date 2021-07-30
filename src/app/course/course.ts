97078417eed3b8d74ad211d01824478758eacf6f
fernandOlle <frolle@inf.ufpel.edu.br>
2021-07-30 00:35:21 -0300

    add list and start component

---
+++ src/app/course/course.ts
@@ -0,0 +1,10 @@
+export class Course {
+    id!: number;
+    name!: string;
+    imageUrl!: string;
+    price!: Number;
+    code!: string;
+    duration!: number;
+    rating!: number;
+    releaseDate!: string;
+}
